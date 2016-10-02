cbf2591ae897862d08238c0f542ec1ddd607b470
Mary <dragonfly_gem@hotmail.com>
2016-10-03 00:55:28 +1100

    Edited Home page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,5 @@ Rails.application.configure do
 
   #needed for devise - remember to change localhost to actual application host
   config.action_mailer.default_url_options = { host: 'localhost', port:3000 }
+
 end
